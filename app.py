2ffeb40728ade0034aa9914323372f56e5bde4b0
Tore Haglund <tore.haglund@gmail.com>
2020-04-18 01:47:43 +0200

    This did CORS before, does it work?

--- app.py
+++ app.py
@@ -81,16 +81,16 @@ def close_connection(exception):
 
 
 ##### CORS ######
-# def add_cors_headers(response):
-#     response.headers['Access-Control-Allow-Origin'] = '*'
-#     if request.method == 'OPTIONS':
-#         response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
-#         headers = request.headers.get('Access-Control-Request-Headers')
-#         if headers:
-#             response.headers['Access-Control-Allow-Headers'] = headers
-#     return response
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response
 
-# app.after_request(add_cors_headers)
+app.after_request(add_cors_headers)
 
 
 
